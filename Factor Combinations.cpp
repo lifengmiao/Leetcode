2d53d0f32c89dc67e3871d95b4890730a99d4536
lifengmiao <lifengmiao@gmail.com>
2018-04-22 22:43:59 -0700

    Create Factor Combinations.cpp

---
+++ Factor Combinations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> getFactors(int n) {
+        vector<vector<int>> res;
+        vector<int> out;
+        helper(n, 2, out, res);
+        return res;
+    }
+    void helper(int n, int start, vector<int> out, vector<vector<int>> &res) {
+        if (n == 1) {
+            if (out.size() > 1) res.push_back(out);
+        } else {
+            for (int i = start; i <= n; ++i) {
+                if (n % i == 0) {
+                    out.push_back(i);
+                    helper(n / i, i, out, res);
+                    out.pop_back();
+                }
+            }
+        }
+    }
+};
