eec06aa08069a710ff585a2cdf972ce176bdd586
lifengmiao <lifengmiao@gmail.com>
2018-04-22 22:46:51 -0700

    add comments

--- Factor Combinations.cpp
+++ Factor Combinations.cpp
@@ -6,14 +6,14 @@ public:
         helper(n, 2, out, res);
         return res;
     }
-    void helper(int n, int start, vector<int> out, vector<vector<int>> &res) {
+    void helper(int n, int start, vector<int>& out, vector<vector<int>> &res) {
         if (n == 1) {
             if (out.size() > 1) res.push_back(out);
         } else {
             for (int i = start; i <= n; ++i) {
                 if (n % i == 0) {
                     out.push_back(i);
-                    helper(n / i, i, out, res);
+                    helper(n / i, i, out, res); //change n and start together
                     out.pop_back();
                 }
             }
