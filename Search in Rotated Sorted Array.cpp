46b8e50c78316af1a314e6668273801a96242398
lifengmiao <lifengmiao@gmail.com>
2018-04-25 21:20:58 -0700

    Update Search in Rotated Sorted Array.cpp

--- Search in Rotated Sorted Array.cpp
+++ Search in Rotated Sorted Array.cpp
@@ -2,6 +2,14 @@
 判断哪边有序使用 nums[mid]<nums[right] 或者 nums[mid]>=nums[left]
 因为当mid 和 left指向同一元素，左边是有序的，而右边是无序的，比如
 [3,1] target=1
+
+注意，因为没有重复元素存在，不会存在nums[mid]==nums[right]，除非mid==right,而这种情况下 left必然也等于mid，即left==mid==right;
+然而mid==left,可能是 mid==left==right-1
+如果有重复元素，if (nums[mid]<nums[right]) 右边有序； if（nums[mid]>nums[right]) 左边有序；if(nums[mid]==nums[right]) 无法确定，只能排除nums[right]
+比如
+3 1 2 3 3 3 3 
+3 3 3 3 1 2 3
+code 见Search in Rotated Sorted Array II
 */
 
 class Solution {
