649bc37e5ca70afbb8737da08c565f40b8ea19eb
lifengmiao <lifengmiao@gmail.com>
2018-04-25 22:07:43 -0700

    Update Binary Search Tree Iterator.cpp

--- Binary Search Tree Iterator.cpp
+++ Binary Search Tree Iterator.cpp
@@ -1,3 +1,13 @@
+/*
+I use Stack to store directed left children from root.
+When next() be called, I just pop one element and process its right child as new root.
+The code is pretty straightforward.
+
+So this can satisfy O(h) memory, hasNext() in O(1) time,
+But next() is O(h) time.
+
+I can't find a solution that can satisfy both next() in O(1) time, space in O(h).
+*/
 class BSTIterator {
     stack<TreeNode *> myStack;
 public:
