74d95f7c4de991f3a6bcae4f8dd99eea45b00668
lifengmiao <lifengmiao@gmail.com>
2018-05-23 22:38:26 -0700

    Update The Maze II.cpp

--- The Maze II.cpp
+++ The Maze II.cpp
@@ -1,4 +1,41 @@
 /*
+其中dists[i][j]表示到达(i,j)这个位置时需要的最小步数，我们都初始化为整型最大值，在后在遍历的过程中不断用较小值
+来更新每个位置的步数值，最后我们来看终点位置的步数值，如果还是整型最大值的话，说明没法在终点处停下来，返回-1，
+否则就返回步数值
+*/
+class Solution {
+public:
+    vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0}};
+    int shortestDistance(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        int m = maze.size(), n = maze[0].size();
+        vector<vector<int>> dists(m, vector<int>(n, INT_MAX)); //distence from start to [i,j]
+        dists[start[0]][start[1]] = 0;
+        helper(maze, start[0], start[1], destination, dists);
+        int res = dists[destination[0]][destination[1]];
+        return (res == INT_MAX) ? -1 : res;
+    }
+    void helper(vector<vector<int>>& maze, int i, int j, vector<int>& destination, vector<vector<int>>& dists) {
+        if (i == destination[0] && j == destination[1]) return;
+        int m = maze.size(), n = maze[0].size();
+        for (auto d : dirs) {
+            int x = i, y = j, dist = dists[x][y];
+            while (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] == 0) {
+                x += d[0];
+                y += d[1];
+                ++dist;
+            }
+            x -= d[0];
+            y -= d[1];
+            --dist;
+            if (dists[x][y] > dist) {
+                dists[x][y] = dist;
+                helper(maze, x, y, destination, dists);
+            }
+        }
+    }
+};
+
+/*
 self code 
 not work!!!!!!!
 */
