14f31d332a366dea2b6a07187730b85265412f24
lifengmiao <lifengmiao@gmail.com>
2018-05-27 11:35:37 -0700

    Update Range Sum Query - Mutable.cpp

--- Range Sum Query - Mutable.cpp
+++ Range Sum Query - Mutable.cpp
@@ -1,39 +1,40 @@
+//https://www.youtube.com/watch?v=v_wj_mOAlig
 class NumArray {
 public:
     NumArray(vector<int> nums) {
-        num.resize(nums.size() + 1); //add 1 to make binary index tree start from 1 (odd number)
-        bit.resize(nums.size() + 1);
-        for (int i = 0; i < nums.size(); ++i) {
+        bit_arr = vector<int> (nums.size()+1, 0); //start from 1 for binary indx tree
+        mNums = vector<int> (nums.size(), 0);
+        for(int i=0; i<nums.size(); i++) {
             update(i, nums[i]);
+            mNums[i] = nums[i];
         }
+        
     }
-    void update(int i, int val) {
-        int diff = val - num[i + 1];
-        for (int j = i + 1; j < bit.size(); j += (j&-j)) {
-            bit[j] += diff;
-            // cout<<j<<endl;
+    
+    void update(int i, int val) {      
+        int diff = val-mNums[i]; //difference from previous value in nums
+        int indx = i+1;
+        while(indx<bit_arr.size()) {
+            bit_arr[indx] += diff;
+            indx += indx&(-indx);
         }
-        num[i + 1] = val;
+        mNums[i] = val; //need to update also for mutiple change in one position
     }
+    
     int sumRange(int i, int j) {
-        return getSum(j + 1) - getSum(i);
-    }    
-    int getSum(int i) { //getSum(i) return sum[0,i-1], not include i, since it has 1 offset
-        int res = 0;
-        for (int j = i; j > 0; j -= (j&-j)) {
-            res += bit[j];
-        }
-        return res;
+        int indx1 = i+1, indx2 = j+1;
+        return range(indx2)-range(indx1-1);
     }
-
+    
+    int range(int indx) {
+        int rst=0;
+        while(indx>0) {
+            rst += bit_arr[indx];
+            indx -= indx&(-indx);
+        }
+        return rst;
+    }
 private:
-    vector<int> num;
-    vector<int> bit;
+    vector<int> bit_arr; //binary indx tree
+    vector<int> mNums; //save curr val in nums to calculate diff
 };
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * obj.update(i,val);
- * int param_2 = obj.sumRange(i,j);
- */
