7a38a17a6572c91790ed11c527a0735df4b2a8c3
lifengmiao <lifengmiao@gmail.com>
2018-05-27 16:19:26 -0700

    Update Sentence Similarity II.cpp

--- Sentence Similarity II.cpp
+++ Sentence Similarity II.cpp
@@ -1,4 +1,73 @@
 /*
+下面这种解法就是碉堡了的联合查找Union Find了，这种解法的核心是一个getRoot函数，如果两个元素属于同一个群组的话，调用
+getRoot函数会返回相同的值。主要分为两部，第一步是建立群组关系，suppose开始时每一个元素都是独立的个体，各自属于不同的群组。
+然后对于每一个给定的关系对，我们对两个单词分别调用getRoot函数，找到二者的祖先结点，如果从未建立过联系的话，那么二者的祖
+先结点时不同的，此时就要建立二者的关系。等所有的关系都建立好了以后，第二步就是验证两个任意的元素是否属于同一个群组，就只
+需要比较二者的祖先结点都否相同啦。是不是有点深度学习的赶脚，先建立模型training，然后再test。哈哈，博主乱扯的，二者并没有
+什么联系。我们保存群组关系的数据结构，有时用数组，有时用哈希map，看输入的数据类型吧，如果输入元素的整型数的话，用root数组
+就可以了，如果是像本题这种的字符串的话，需要用哈希表来建立映射，建立每一个结点和其祖先结点的映射。注意这里的祖先结点不一定
+是最终祖先结点，而最终祖先结点的映射一定是最重祖先结点，所以我们的getRoot函数的设计思路就是要找到最终祖先结点，那么就是当
+结点和其映射结点相同时返回，否则继续循环，可以递归写，也可以迭代写，这无所谓。注意这里第一行判空是相当于初始化，这个操作可
+以在外面写，就是要让初始时每个元素属于不同的群组，参见代码如下
+*/
+class Solution {
+public:
+    bool areSentencesSimilarTwo(vector<string>& words1, vector<string>& words2, vector<pair<string, string>> pairs) {
+        if(words1.size()!=words2.size()) return false;
+        unordered_map<string, string> mMap;
+        for(auto it:pairs) {
+            string x = getRoot(mMap, it.first);
+            string y = getRoot(mMap, it.second);
+            if(x!=y) mMap[x]=y;
+        }
+        for(int i=0; i<words1.size(); i++) {
+            if(getRoot(mMap, words1[i])!=getRoot(mMap, words2[i])) return false;
+        }
+        return true;
+    }
+    
+    string getRoot(unordered_map<string, string>& mMap, string word) {
+        if(!mMap.count(word)) mMap[word]=word;
+        return mMap[word]==word?word:getRoot(mMap, mMap[word]);
+    }
+};
+/*
+另外一种写法
+*/
+class Solution {
+public:
+    bool areSentencesSimilarTwo(vector<string>& words1, vector<string>& words2, vector<pair<string, string>> pairs) {
+        if(words1.size()!=words2.size()) return false;
+        unordered_map<string, string> mMap;
+        for(auto it:pairs) {
+            string x,y;
+            if(!mMap.count(it.first)) {
+                mMap[it.first] = it.first;
+                x = it.first;
+            }
+            else {
+                x = getRoot(mMap, it.first);
+            }
+            if(!mMap.count(it.second)) {
+                mMap[it.second] = it.second;
+                y = it.second;
+            }
+            else {
+                y = getRoot(mMap, it.second);
+            }
+            if(x!=y) mMap[x]=y;
+        }
+        for(int i=0; i<words1.size(); i++) {
+            if(getRoot(mMap, words1[i])!=getRoot(mMap, words2[i])) return false;
+        }
+        return true;
+    }
+    
+    string getRoot(unordered_map<string, string>& mMap, string word) {
+        return mMap[word]==word?word:getRoot(mMap, mMap[word]);
+    }
+};
+/*
 这道题是之前那道Sentence Similarity的拓展，那道题说单词之间不可传递，
 于是乎这道题就变成可以传递了，那么难度就增加了。不过没有关系，还是用我
 们的经典老三样来解，BFS，DFS，和Union Find。我们先来看BFS的解法，其
