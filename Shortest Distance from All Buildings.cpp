85e3d86697cc3ff8683cc4c5333feb975d204fc4
lifengmiao <lifengmiao@gmail.com>
2018-05-29 23:22:42 -0700

    Update Shortest Distance from All Buildings.cpp

--- Shortest Distance from All Buildings.cpp
+++ Shortest Distance from All Buildings.cpp
@@ -8,6 +8,9 @@ BFS
 将其赋为-1，这样下一轮遍历我们就找-1的位置，然后将其都赋为-2，以此类推直至遍历完所有的建筑物，然后在遍历的过程中更新dist和sum的值，
 注意我们的dist算是个局部变量，每次都初始化为grid，真正的距离场累加在sum中，由于建筑的位置在grid中是1，所以dist中初始化也是1，
 累加到sum中就需要减1，我们用sum中的值来更新结果res的值，最后根据res的值看是否要返回-1，参见代码如下：
+
+notes: 和wall and gates 的不同在于，wall and gates首先把所有的gates push到queue中，因为他是求最小值，不需要每个gate建立一个dist，
+只需要在扫的过程中更新为最小的值即可；而本题是求和，所以每个建筑‘1’，都要建立一个dist
 */
 
 class Solution {
@@ -42,5 +45,27 @@ public:
             }
         }
         return res == INT_MAX ? -1 : res;
+    }
+};
+//Wall and gates
+class Solution {
+public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        queue<pair<int, int>> q;
+        vector<vector<int>> dirs{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+        for (int i = 0; i < rooms.size(); ++i) {
+            for (int j = 0; j < rooms[i].size(); ++j) {
+                if (rooms[i][j] == 0) q.push({i, j});   
+            }
+        }
+        while (!q.empty()) {
+            int i = q.front().first, j = q.front().second; q.pop();
+            for (int k = 0; k < dirs.size(); ++k) {
+                int x = i + dirs[k][0], y = j + dirs[k][1];
+                if (x < 0 || x >= rooms.size() || y < 0 || y >= rooms[0].size() || rooms[x][y] < rooms[i][j] + 1) continue;
+                rooms[x][y] = rooms[i][j] + 1;
+                q.push({x, y});
+            }
+        }
     }
 };
