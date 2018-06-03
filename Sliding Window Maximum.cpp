4b083242184a237c27ea74b976303267df0f954c
lifengmiao <lifengmiao@gmail.com>
2018-06-03 11:13:26 -0700

    Create Sliding Window Maximum.cpp

---
+++ Sliding Window Maximum.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        deque<int> q; //只存window中 需要存的元素
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!q.empty() && q.front() == i - k) //超出window，remove one element
+                q.pop_front();
+            while (!q.empty() && nums[q.back()] < nums[i]) //小于当前元素的都删除， 这样做其实还可以保证dq中的元素是按从大到小顺序排列的
+                q.pop_back();
+            q.push_back(i);
+            if (i >= k - 1) res.push_back(nums[q.front()]);
+        }
+        return res;
+    }
+};
