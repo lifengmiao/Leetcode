41abc1e9e1baab827d7c75081a60c3b651305613
lifengmiao <lifengmiao@gmail.com>
2018-06-03 11:19:28 -0700

    Update Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -3,6 +3,7 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> res;
+        //queue中为 [当前window中最大元素的位置，在当前window中最大元素位置之后次大元素的位置，。。。]
         deque<int> q; //存的是元素的indx，不是元素本身，只存window中需要存的元素的indx
         for (int i = 0; i < nums.size(); ++i) {
             if (!q.empty() && q.front() == i - k) //超出window，remove one element
