54d3037603252ea64f988d80bf16bd812742b787
lifengmiao <lifengmiao@gmail.com>
2018-06-16 23:06:07 -0700

    Update Binary Tree Longest Consecutive Sequence II.cpp

--- Binary Tree Longest Consecutive Sequence II.cpp
+++ Binary Tree Longest Consecutive Sequence II.cpp
@@ -1,3 +1,7 @@
+/*
+这种穿过root的问题，dfs返回的值和最终结果需要的值不同，dfs返回的值到current root为止，不会穿过root再往另一边走，可以理解为
+单条的。而最终结果需要的值，是双条的。这种结构，dfs需要传一个refer 存结果res，返回值用于计算res，返回值并不是res
+*/
 class Solution {
 public:
     int longestConsecutive(TreeNode* root) {
