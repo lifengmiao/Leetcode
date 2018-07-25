916b19570cc04cdd060e26a16b2492db4de0fd61
lifengmiao <lifengmiao@gmail.com>
2018-07-24 21:06:30 -0700

    Update and rename clone graph to clone graph.cpp

---
+++ clone graph.cpp
@@ -0,0 +1,46 @@
+//DFS
+class Solution {
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        unordered_map<int, UndirectedGraphNode*> Map;
+        return clone(node, Map);
+    }
+    
+    UndirectedGraphNode* clone(UndirectedGraphNode *node, unordered_map<int, UndirectedGraphNode*>& Map) {
+        if(!node) return NULL;
+        if(Map.count(node->label)) return Map[node->label];
+        UndirectedGraphNode* new_node = new UndirectedGraphNode(node->label);
+        Map[node->label] = new_node;
+        for(int i=0; i<node->neighbors.size(); i++) {
+            UndirectedGraphNode* new_neighbor = clone(node->neighbors[i], Map);
+            (new_node->neighbors).push_back(new_neighbor);
+        }
+        return new_node;
+    }
+};
+//BFS
+class Solution {
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if(!node) return NULL;
+        queue<UndirectedGraphNode *> mQ;
+        unordered_map<UndirectedGraphNode *, UndirectedGraphNode *> hash;
+        UndirectedGraphNode * head = new UndirectedGraphNode(node->label);
+        hash[node] = head;
+        mQ.push(node);
+        while(!mQ.empty()) {
+            UndirectedGraphNode * cur = mQ.front();
+            mQ.pop();
+            for(UndirectedGraphNode * it:cur->neighbors) {
+                if(hash.count(it)) hash[cur]->neighbors.push_back(hash[it]);
+                else {
+                    UndirectedGraphNode * new_neighbor = new UndirectedGraphNode(it->label);
+                    hash[cur]->neighbors.push_back(new_neighbor);
+                    hash[it] = new_neighbor;
+                    mQ.push(it);
+                }
+            }            
+        }
+        return head;
+    }
+};
