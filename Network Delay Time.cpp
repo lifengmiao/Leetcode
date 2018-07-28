95e779ae6a5a7f259522cdd0e041de13d60e69ab
lifengmiao <lifengmiao@gmail.com>
2018-07-27 22:31:33 -0700

    Update Network Delay Time.cpp

--- Network Delay Time.cpp
+++ Network Delay Time.cpp
@@ -1,3 +1,4 @@
+//BFS
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int N, int K) {
@@ -26,5 +27,31 @@ public:
             res = max(res, dist[i]);
         }
         return res == INT_MAX ? -1 : res;
+    }
+};
+
+//DFS (time limit exceeded)
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+        int res = 0;
+        vector<vector<int>> edges(101, vector<int>(101, -1));
+        vector<int> dist(N + 1, INT_MAX);
+        for (auto e : times) edges[e[0]][e[1]] = e[2];
+        dfs(K, edges, dist, 0);
+        for (int i = 1; i <= N; ++i) {
+            res = max(res, dist[i]);
+        }
+        return res == INT_MAX ? -1 : res;
+    }
+    
+    void dfs(int cur, vector<vector<int>>& edges, vector<int>& dist, int delay) {
+        if(dist[cur]<delay) return;
+        dist[cur]=delay;
+        for(int i=0; i<edges[cur].size(); i++){
+            if(edges[cur][i]!=-1) {
+                dfs(i, edges, dist, delay+edges[cur][i]);
+            }
+        }
     }
 };
