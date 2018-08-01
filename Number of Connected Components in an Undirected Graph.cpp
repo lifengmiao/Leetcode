1fef9a621f7a1f02d872b2d6b5a1672e7961527a
lifengmiao <lifengmiao@gmail.com>
2018-07-31 21:13:00 -0700

    Update Number of Connected Components in an Undirected Graph.cpp

--- Number of Connected Components in an Undirected Graph.cpp
+++ Number of Connected Components in an Undirected Graph.cpp
@@ -19,3 +19,36 @@ public:
         return root[i]==i?i:findRoot(root, root[i]);
     }
 };
+//DFS
+class Solution {
+public:
+    int countComponents(int n, vector<pair<int, int>>& edges) {
+        unordered_set<int> visit;
+        unordered_map<int, unordered_set<int>> graph;
+        int res = 0;
+        for(int i=0; i<edges.size(); i++) {
+            graph[edges[i].first].insert(edges[i].second);
+            graph[edges[i].second].insert(edges[i].first);
+        }
+        for(int i = 0; i<n; i++) {
+                // cout<<"start "<<(it->first)<<endl;
+                if(!visit.count(i)) {
+                    res ++;
+                    dfs(i, graph, visit);
+                }
+                    
+        }
+        return res;
+    }
+    
+    void dfs(int i, unordered_map<int, unordered_set<int>>& graph, unordered_set<int>& visit) {
+        if(visit.count(i)) return;
+        
+        visit.insert(i);
+        for(auto it:graph[i]) {
+            // cout<<"inside "<<it<<endl;
+            dfs(it, graph, visit);
+        }
+            
+    }
+};
